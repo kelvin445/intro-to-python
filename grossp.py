24cf585890cfd0a6f06472155abb14193b2d0450
Granted <122792142+kelvin445@users.noreply.github.com>
2023-01-23 12:54:46 +0300

    Add files via upload

---
+++ grossp.py
@@ -0,0 +1,7 @@
+#program to compute gross pay
+hours_worked=int(input("Enter the number of hours:"))
+rate_per_hour=float(input("Enter the rate:"))
+gross_pay=hours_worked*rate_per_hour
+print(hours_worked)
+print(rate_per_hour)
+print(gross_pay)
